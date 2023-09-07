8a91c291905a8252027bfcb88067d397b7e659f3
rafaeloduarte <rafaeloduarte@gmail.com>
2023-09-06 23:42:48 -0300

    feat: drag and drop

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -1,6 +1,59 @@
-import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { useReactTable, flexRender, getCoreRowModel } from "@tanstack/react-table";
+import { useDrag, useDrop } from 'react-dnd'
 import { useMemo } from "react";
 import mData from '../MOCK_DATA.json'
+
+const reorderColumn = (draggedColumnId, targetColumnId, columnOrder) => {
+    columnOrder.splice(
+        columnOrder.indexOf(targetColumnId),
+        0,
+        columnOrder.splice(columnOrder.indexOf(draggedColumnId), 1)[0]
+    )
+    return [...columnOrder]
+}
+
+
+const DraggableColumnHeader = ({ header, table }) => {
+    const { getState, setColumnOrder } = table
+    const { columnOrder } = getState()
+    const { column } = header
+
+    const [, dropRef] = useDrop({
+        accept: 'column',
+        drop: (draggedColumn) => {
+            const newColumnOrder = reorderColumn(
+                draggedColumn.id,
+                column.id,
+                columnOrder
+            )
+            setColumnOrder(newColumnOrder)
+        },
+    })
+
+    const [{ isDragging }, dragRef, previewRef] = useDrag({
+        collect: monitor => ({
+            isDragging: monitor.isDragging(),
+        }),
+        item: () => column,
+        type: 'column',
+    })
+
+    return (
+        <th
+            ref={dropRef}
+            colSpan={header.colSpan}
+            style={{ opacity: isDragging ? 0.5 : 1 }}
+        >
+            <div ref={previewRef}>
+                {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.header, header.getContext())}
+                <button ref={dragRef}>🟰</button>
+            </div>
+        </th>
+    )
+}
+
 
 function BasicTable() {
 
@@ -45,14 +98,12 @@ function BasicTable() {
                 <thead>
                     {table.getHeaderGroups().map(headerGroup => (
                         <tr key={headerGroup.id}>
-                            {headerGroup.headers.map(header => (
-                                <th key={header.id}>
-                                    {flexRender(
-                                        header.column.columnDef.header,
-                                        header.getContext()
-                                    )}
-                                </th>
-                            )
+                            {headerGroup.headers.map(header =>
+                                <DraggableColumnHeader
+                                    key={header.id}
+                                    header={header}
+                                    table={table}
+                                />
                             )}
                         </tr>
                     ))}
